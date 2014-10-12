6dd423be870981c50e56185ff7ab0244c640e75f
phracker <phracker@bk.ru>
2014-10-11 23:18:04 -0400

    Remove dialog dependency

--- make_tarballs.sh
+++ make_tarballs.sh
@@ -6,36 +6,35 @@ TARGET="$(pwd)";
 LOGFILE="$(mktemp)";
 
 log() {
-  dialog --infobox "$@" 4 80;
+  log "$@" 2>&1 | tee "$LOGFILE";
 }
 
-echo "Directory: $TARGET";
-echo "Log: $LOGFILE";
-
-if [[ -f "$(which dialog)" ]]; then
-  echo "Dialog installed at $(which dialog).";
-else
-  echo "Dialog not installed. Please install and try again.";
-  exit;
-fi
+log "Current Directory: $TARGET";
+log "Log: $LOGFILE";
 
 if [[ $# -eq 0 ]]; then
-  echo "Building MacOSX-SDKs release tarballs.";
+  log "Building release tarballs.";
 else
-  echo "This script does not take arguments.";
+  log "This script does not take arguments.";
   exit;
 fi
 
 if [[ ! -d "$TARGET/release/" ]]; then
-  echo "Making release directory at $TARGET/release/";
+  log "Making release directory at $TARGET/release/";
   mkdir "$TARGET/release/";
 else
-  dialog --title "Releases Directory" --yesno "An old release directory was found. Would you like to delete it before proceeding?" 10 60;
-  echo "\$?: $?";
-  if [[ "$?" -eq "0" ]]; then
-    echo "Removing old release directory at $TARGET/release/";
+  echo -n "An old release directory was found. Would you like to delete it before proceeding? [Y/n]: ";
+  read DELETE_RELEASES;
+  echo "";
+  if [[ ! "$DELETE_RELEASES" -eq "n" ]]; then
+    log "Removing old release directory at $TARGET/release/";
     rm -rf "$TARGET/release/";
-    echo "Making release directory at $TARGET/release/";
+    log "Making release directory at $TARGET/release/";
+    mkdir "$TARGET/release/";
+  else
+    log "Moving old release directory to $TARGET/release_backup/";
+    mv "$TARGET/release/" "$TARGET/release_backup/";
+    log "Making release directory at $TARGET/release/";
     mkdir "$TARGET/release/";
   fi
 fi
@@ -45,4 +44,5 @@ for sdk_directory in *.sdk; do
   tar -cpJf "release/$sdk_directory.tar.xz" "$sdk_directory";
 done
 
-du -h "$TARGET/release/"* | sed s/release\\\///g;
+log "Done. Contents of $TARGET/release/:";
+du -h "$TARGET/release/"* | sed s/release\\\///g 2>&1 | tee "$LOGFILE";
