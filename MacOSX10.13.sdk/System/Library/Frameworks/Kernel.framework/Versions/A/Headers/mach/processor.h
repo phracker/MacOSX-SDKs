59bbcee2d114ca18ac06a9d6dc820baf53136caa
Christopher Larson <kergoth@gmail.com>
2018-09-08 16:02:24 -0700

    MacOSX10.13.sdk: fix numerous broken links to Users/phracker/...

--- MacOSX10.13.sdk/System/Library/Frameworks/Kernel.framework/Versions/A/Headers/mach/processor.h
+++ MacOSX10.13.sdk/System/Library/Frameworks/Kernel.framework/Versions/A/Headers/mach/processor.h
@@ -1,1 +1,1 @@
-Users/phracker/Documents/Xcode-beta.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/mach/processor.h
+../../../../../../../../usr/include/mach/processor.h
